bfd706ba49052d61b1550ae74109b856238cd707
Hesnan Ávila <avilahesnan@outlook.com>
2024-02-21 16:04:36 -0300

    feat: add pagination and filters

--- apps/client/admin.py
+++ apps/client/admin.py
@@ -4,9 +4,10 @@ from apps.client.models import Client
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'active',)
-    list_display_links = ('id', 'name',)
-    search_fields = ('name',)
+    list_display = ('id', 'name', 'cpf', 'active',)
+    list_display_links = ('id', 'name', 'cpf',)
+    search_fields = ('name', 'cpf',)
     list_filter = ('active',)
     list_editable = ('active',)
     list_per_page = 10
+    ordering = ('name',)
--- apps/client/views.py
+++ apps/client/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from apps.client.serializer import ClientSerializer
 from apps.client.models import Client
 
@@ -13,3 +14,7 @@ class ClientsViewSet(viewsets.ModelViewSet):
 
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]  # noqa: E501
+    ordering_fields = ['name']
+    search_fields = ['name', 'cpf']
+    filterset_fields = ['active']
